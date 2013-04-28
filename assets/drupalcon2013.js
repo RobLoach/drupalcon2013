b2d78ef7de5960e5d763f3541e8f5b496b019d11
Rob Loach <robloach@gmail.com>
2013-04-28 19:28:33 -0400

    Add more slides

--- assets/drupalcon2013.js
+++ assets/drupalcon2013.js
@@ -1,4 +1,4 @@
-require(['revealjs'], function(Reveal) {
+require(['revealjs', 'highlightjs'], function(Reveal, hljs) {
       // Full list of configuration options available here:
       // https://github.com/hakimel/reveal.js#configuration
       Reveal.initialize({
@@ -6,20 +6,7 @@ require(['revealjs'], function(Reveal) {
         progress: true,
         history: true,
         center: true,
-
-        //theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-        transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
-
-        // Optional libraries used to extend on reveal.js
-        /*dependencies: [
-          { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
-          { src: 'plugin/markdown/showdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-          { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-          { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-          { src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-          { src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
-          // { src: 'plugin/remotes/remotes.js', async: true, condition: function() { return !!document.body.classList; } }
-        ]*/
+        transition: Reveal.getQueryHash().transition || 'default'
       });
       hljs.initHighlighting();
 });
